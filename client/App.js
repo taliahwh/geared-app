0f707e2b4d49d36dfc0739bc846aad919d8067a4
Taliah Wharton <taliahwh23@yahoo.com>
2022-06-02 16:47:07 -0400

    app transfer setup

--- client/App.js
+++ client/App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+// Navigators
+import MainNavigator from './src/navigation/MainNavigator';
+import AuthNavigator from './src/navigation/AuthNavigator';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <MainNavigator /> */}
+      <AuthNavigator />
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
