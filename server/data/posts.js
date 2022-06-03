12896a7c240d96f639983e7c184b1b2187f8d863
Taliah Wharton <taliahwh23@yahoo.com>
2022-06-03 16:15:35 -0400

    axios -> server connection

---
+++ server/data/posts.js
@@ -0,0 +1,62 @@
+const posts = [
+  {
+    images: [
+      {
+        title: 'KD_front.jpg',
+        imgUrl: 'https://i.ebayimg.com/images/g/QXQAAOSw8ZhiepHC/s-l600.jpg',
+      },
+      {
+        title: 'KD_back.jpg',
+        imgUrl: 'https://i.ebayimg.com/images/g/e7QAAOSwcrdiepHD/s-l600.jpg',
+      },
+    ],
+    description:
+      '2015/16 Panini Flawless Auto Sapphire Kevin Durant 10/10 Panini Sealed',
+    listedBy: { username: 'fab__five' },
+  },
+  // {
+  //   images: [
+  //     'https://i.ebayimg.com/images/g/hoEAAOSwnk9ijqfE/s-l600.jpg',
+  //     'https://i.ebayimg.com/images/g/Go4AAOSwLQFijqfI/s-l600.jpg',
+  //   ],
+  //   description:
+  //     '2018-19 Panini Certified Rookie Roll Call Luka Doncic RC On Card AUTO PSA 9',
+  //   listedBy: { username: 'taliahwh' },
+  // },
+  // {
+  //   images: [
+  //     'https://i.ebayimg.com/images/g/BjYAAOSwgVJihS4R/s-l600.jpg',
+  //     'https://i.ebayimg.com/images/g/Kf0AAOSwQBJihS4T/s-l600.jpg',
+  //     'https://i.ebayimg.com/images/g/3gwAAOSw8PtihS4u/s-l600.jpg',
+  //   ],
+  //   description:
+  //     '2015-16 Devin Booker Panini Limited Spotlight Rookie Patch Auto 2/49 Suns',
+  //   listedBy: { username: 'hawks123' },
+  //   showcase: false,
+  //   openToOffers: true,
+  // },
+  // {
+  //   images: [
+  //     'https://i.ebayimg.com/images/g/XrkAAOSwtr9guPyr/s-l600.jpg',
+  //     'https://i.ebayimg.com/images/g/moAAAOSwDhhguPyw/s-l600.jpg',
+  //   ],
+  //   description:
+  //     'KOBE BRYANT / KEVIN GARNETT 2002 SP GAME EDITION USED DUAL JERSEY /100 NBA HOF',
+  //   listedBy: { username: '4crispcorners' },
+  // },
+  // {
+  //   images: [
+  //     'https://i.ebayimg.com/images/g/W-wAAOSwPHJh2eA~/s-l600.jpg',
+  //     'https://i.ebayimg.com/images/g/dPoAAOSwAmJh2eBU/s-l600.jpg',
+  //     'https://i.ebayimg.com/images/g/8XEAAOSwKSRh2eBh/s-l600.jpg',
+  //   ],
+  //   description:
+  //     'Stephen Steph Curry PANINI HTF GOLDEN STATE REVOLUTION CARD - MINT - W/ CASE',
+  //   listedBy: { username: 'fab__five' },
+  //   showcase: false,
+  //   forSale: true,
+  //   price: 50,
+  // },
+];
+
+export default posts;
