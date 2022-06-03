379228b7a5311e2d06e3734676e13b29491f13fc
Taliah Wharton <taliahwh23@yahoo.com>
2022-06-03 16:15:46 -0400

    axios -> server connection

---
+++ server/models/postModel.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+
+export const postSchema = new mongoose.Schema(
+  {
+    images: {
+      type: [
+        {
+          title: {
+            type: String,
+            required: true,
+          },
+          imgUrl: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    showcase: {
+      type: Boolean,
+      default: true,
+    },
+    forSale: {
+      type: Boolean,
+      default: false,
+    },
+    openToOffers: {
+      type: Boolean,
+      default: false,
+    },
+    likes: {
+      type: [String],
+      default: [],
+    },
+    comments: {
+      type: [String],
+      default: [],
+    },
+    listedBy: {
+      username: {
+        type: String,
+        required: true,
+      },
+      // userId: {
+      //   type: mongoose.Schema.Types.ObjectId,
+      //   required: true,
+      // },
+    },
+  },
+  {
+    // Automatically generates createdAt and updatedAt fields
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
