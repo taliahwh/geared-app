49de0802d3a146bac4bd6b1fa5f40d8227db468f
Taliah Wharton <taliahwh23@yahoo.com>
2022-06-14 14:45:33 -0400

    whitelisted user signin, added userDetails & userPost

--- client/src/store.js
+++ client/src/store.js
@@ -7,20 +7,30 @@ import { composeWithDevTools } from '@redux-devtools/extension';
 import {
   createPostReducer,
   explorePostsReducer,
+  postDetailsReducer,
 } from './reducers/postReducers';
 
-import { userSignInReducer, userSignUpReducer } from './reducers/userReducers';
+import {
+  userDetailsReducer,
+  userPostsReducer,
+  userSignInReducer,
+  userSignUpReducer,
+} from './reducers/userReducers';
 
 const reducer = combineReducers({
   explorePosts: explorePostsReducer,
   createPost: createPostReducer,
+  postDetails: postDetailsReducer,
   userSignIn: userSignInReducer,
   userSignUp: userSignUpReducer,
+  userDetails: userDetailsReducer,
+  userPosts: userPostsReducer,
 });
 
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
+  whitelist: ['userSignIn'],
 };
 
 const persistedReducer = persistReducer(persistConfig, reducer);
