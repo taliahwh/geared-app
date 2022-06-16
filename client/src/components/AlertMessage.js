35b05993c73722ddb0df8e9a8d24800eda8d70b3
Taliah Wharton <taliahwh23@yahoo.com>
2022-06-16 17:08:41 -0400

    changed component name

---
+++ client/src/components/AlertMessage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const AlertMessage = ({ children }) => {
+  return <Text style={styles.alert}>{children}</Text>;
+};
+
+const styles = StyleSheet.create({
+  alert: {
+    // backgroundColor: '#f87171',
+    color: '#f87171',
+    paddingTop: 20,
+    paddingBottom: 7,
+    width: '100%',
+    marginBottom: 10,
+    textAlign: 'center',
+    fontWeight: '500',
+  },
+});
+
+export default AlertMessage;
