61c8e99c3e1ce68a047e99a86f68df1d2b8f7748
Taliah Wharton <taliahwh23@yahoo.com>
2022-06-17 16:34:37 -0400

    changed screen name

--- client/src/navigation/HomeScreenNavigator.js
+++ client/src/navigation/HomeScreenNavigator.js
@@ -24,7 +24,7 @@ const HomeScreenNavigator = () => {
         }}
       />
       <Stack.Screen
-        name="User Profile"
+        name="Profile Details"
         component={ViewUserProfileScreen}
         options={{
           headerTitle: '',
