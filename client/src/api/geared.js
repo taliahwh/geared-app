4600235f5079896626aef1647354fddc1ad0e997
Taliah Wharton <taliahwh23@yahoo.com>
2022-06-21 15:25:14 -0400

    ngrok update

--- client/src/api/geared.js
+++ client/src/api/geared.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://97e7-2601-4a-c200-d710-4cbc-bce1-e7d8-4be0.ngrok.io',
+  baseURL: 'https://0ea7-2601-4a-c200-d710-4cbc-bce1-e7d8-4be0.ngrok.io',
 });
