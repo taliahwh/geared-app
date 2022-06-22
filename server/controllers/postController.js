9501835699202f2b7c61d29481f155a475c2f4ab
Taliah Wharton <taliahwh23@yahoo.com>
2022-06-22 14:52:15 -0400

    like post workflow

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import asyncHandler from 'express-async-handler';
 
 import Post from '../models/postModel.js';
@@ -82,4 +83,59 @@ const getPostById = asyncHandler(async (req, res) => {
   res.status(200).json(post);
 });
 
-export { getAllPosts, createNewPost, getPostById };
+/**
+ * @desc Like post by post id
+ * @route PUT /posts/:id/likepost
+ * @access Public
+ */
+const likePost = asyncHandler(async (req, res) => {
+  const { id: postId } = req.params;
+  const { id: userId } = req.user;
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    res.status(404);
+    throw new Error('Nost authorized. Must be logged in to like post.');
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(postId)) {
+    res.status(404);
+    throw new Error('No post with that id');
+  }
+
+  const post = await Post.findById(postId);
+
+  /* checks the index to see if the userId already appears in the post's likes */
+  const index = post.likes.findIndex((id) => id === String(userId));
+
+  if (index === -1) {
+    // like the post
+    post.likes.push(userId);
+  } else {
+    // dislike the post
+    // returns an array of likes without the current user's like (removes)
+    post.likes = post.likes.filter((id) => id !== String(userId));
+  }
+
+  const updatedPost = await post.save();
+
+  // Updating liked posts arr in User model
+  const postExistsInLikedPosts = user.likedPosts.includes(
+    String(updatedPost._id)
+  ); // returns Boolean
+
+  if (!postExistsInLikedPosts) {
+    user.likedPosts.push(updatedPost._id); // if post is not in arr, pushed post's id
+  }
+  if (postExistsInLikedPosts) {
+    user.likedPosts = user.likedPosts.filter(
+      (id) => id !== String(updatedPost._id)
+    );
+  }
+
+  await user.save();
+  res.status(200).json(updatedPost);
+});
+
+export { getAllPosts, createNewPost, getPostById, likePost };
