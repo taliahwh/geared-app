4ad2a7b76dcf99c2c1127ba1ad8ec1594b1597af
Taliah Wharton <taliahwh23@yahoo.com>
2022-06-23 18:00:22 -0400

    saved posts workflow

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -138,4 +138,96 @@ const likePost = asyncHandler(async (req, res) => {
   res.status(200).json(updatedPost);
 });
 
-export { getAllPosts, createNewPost, getPostById, likePost };
+/**
+ * @desc Fetch all user's liked posts
+ * @route GET /posts/likedposts
+ * @access Public
+ */
+const getLikedPosts = asyncHandler(async (req, res) => {
+  const { id: userId } = req.user;
+  const user = await User.findById(userId);
+
+  if (!user) {
+    res.status(404);
+    throw new Error('Not authorized. Please sign in.');
+  }
+
+  const userLikes = user.likedPosts.map(mongoose.Types.ObjectId); // convert strs to objectId
+  const likedPosts = await Post.find({ _id: { $in: userLikes } });
+  const likedPostsInOrder = likedPosts.reverse();
+
+  res.status(200).json(likedPostsInOrder);
+});
+
+/**
+ * @desc Save post by post id
+ * @route PUT /posts/:id/savepost
+ * @access Public
+ */
+const savePost = asyncHandler(async (req, res) => {
+  const { id: postId } = req.params;
+  const { id: userId } = req.user;
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    res.status(404);
+    throw new Error('Nost authorized. Must be logged in to save post.');
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(postId)) {
+    res.status(404);
+    throw new Error('No post with that id');
+  }
+
+  const postToSave = await Post.findById(postId);
+
+  // Updating saved posts arr in User model
+  const postExistsInSavedPosts = user.savedPosts.includes(
+    String(postToSave._id)
+  ); // returns Boolean
+
+  if (!postExistsInSavedPosts) {
+    user.savedPosts.push(postToSave._id); // if post is not in arr, pushed post's id
+  }
+  if (postExistsInSavedPosts) {
+    user.savedPosts = user.savedPosts.filter(
+      (id) => id !== String(postToSave._id)
+    );
+  }
+
+  await user.save();
+  // const updatedSavedPosts = user.savedPosts;
+  res.status(200).json(user);
+});
+
+/**
+ * @desc Fetch all user's saved posts
+ * @route GET /posts/savedposts
+ * @access Public
+ */
+const getSavedPosts = asyncHandler(async (req, res) => {
+  const { id: userId } = req.user;
+  const user = await User.findById(userId);
+
+  if (!user) {
+    res.status(404);
+    throw new Error('Not authorized. Please sign in.');
+  }
+
+  const userSavedPosts = user.savedPosts.map(mongoose.Types.ObjectId); // convert strs to objectId
+  const savedPosts = await Post.find({ _id: { $in: userSavedPosts } });
+  const savedPostsInOrder = savedPosts.reverse();
+
+  res.status(200).json(savedPostsInOrder);
+});
+
+export {
+  getAllPosts,
+  createNewPost,
+  getPostById,
+  likePost,
+  getLikedPosts,
+  savePost,
+  getSavedPosts,
+};
