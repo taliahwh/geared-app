85cb31190201bc1af2f26742e31a1db71d2f1951
Taliah Wharton <taliahwh23@yahoo.com>
2022-06-27 15:44:34 -0400

    added notifications bell as header btn

--- client/src/navigation/ProfileNavigator.js
+++ client/src/navigation/ProfileNavigator.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
 
 import ProfileScreen from '../screens/ProfileScreen';
 import PostDetailsScreen from '../screens/PostDetailsScreen';
+import NotificationsScreen from '../screens/NotificationsScreen';
+
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 
 const ProfileNavigator = () => {
+  const navigation = useNavigation();
   return (
     <Stack.Navigator options={{ headerBackTitleVisible: false }}>
       <Stack.Screen
@@ -18,14 +24,32 @@ const ProfileNavigator = () => {
             elevation: 0,
             shadowOpacity: 0,
             borderBottomWidth: 0,
-            height: 60,
+            // height: 60,
           },
+          headerLeft: () => (
+            <View style={{ marginLeft: 15 }}>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={() => navigation.navigate('Notifications')}
+              >
+                <Ionicons name="ios-notifications" size={25} color="black" />
+              </TouchableOpacity>
+            </View>
+          ),
         }}
       />
       <Stack.Screen
         name="User Post Details"
         component={PostDetailsScreen}
         options={{ headerTitle: '', headerBackTitleVisible: false }}
+      />
+      <Stack.Screen
+        name="Notifications"
+        component={NotificationsScreen}
+        options={{
+          headerTitle: 'Notifications',
+          headerBackTitleVisible: false,
+        }}
       />
     </Stack.Navigator>
   );
