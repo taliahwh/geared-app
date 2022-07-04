fe99ab8d2705030d8c86be96328d25637d384753
Taliah Wharton <taliahwh23@yahoo.com>
2022-07-04 17:16:08 -0400

    component cleanup

--- client/src/components/tab-view/CollectionRoute.js
+++ client/src/components/tab-view/CollectionRoute.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+// import React from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
 
-// Components
-import ProfileHeader from '../ProfileHeader';
-import CollectionGrid from '../CollectionGrid';
+// // Components
+// import ProfileHeader from '../ProfileHeader';
+// import CollectionGrid from '../CollectionGrid';
 
-const CollectionRoute = () => {
-  return (
-    <View style={styles.collectionContainter}>
-      <ProfileHeader />
-      <CollectionGrid />
-    </View>
-  );
-};
+// const CollectionRoute = () => {
+//   return (
+//     <View style={styles.collectionContainter}>
+//       <ProfileHeader />
+//       <CollectionGrid />
+//     </View>
+//   );
+// };
 
-const styles = StyleSheet.create({
-  collectionContainter: {
-    flex: 1,
-  },
-});
+// const styles = StyleSheet.create({
+//   collectionContainter: {
+//     flex: 1,
+//   },
+// });
 
-export default CollectionRoute;
+// export default CollectionRoute;
