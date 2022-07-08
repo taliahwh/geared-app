130505521dc641093449cbb573bbee5156dced59
Taliah Wharton <taliahwh23@yahoo.com>
2022-07-08 15:17:47 -0400

    passed props into ProfileHeader component

--- client/src/screens/ViewUserProfileScreen.js
+++ client/src/screens/ViewUserProfileScreen.js
@@ -1,16 +1,57 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Dimensions, StatusBar } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, StatusBar } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Components
 import ProfileHeader from '../components/ProfileHeader';
 import CollectionGrid from '../components/CollectionGrid';
+import ProfileHeaderLoader from '../components/Loaders/ProfileHeaderLoader';
+import AlertMessage from '../components/AlertMessage';
+
+// Actions
+import { getUserDetails } from '../actions/userActions';
 
 const ViewUserProfileScreen = ({ route }) => {
   const { userId } = route.params;
 
+  // Hooks
+  const dispatch = useDispatch();
+
+  // State from redux
+  const { _id: authUserId } = useSelector((state) => state.userSignIn.userInfo);
+  const {
+    loading: loadingUserDetails,
+    userDetails,
+    error: errorUserDetails,
+  } = useSelector((state) => state.userDetails);
+
+  const { success: successFollowUser } = useSelector(
+    (state) => state.followUser
+  );
+
+  useEffect(() => {
+    dispatch(getUserDetails(userId));
+  }, [dispatch, successFollowUser]);
+
   return (
     <View style={styles.collectionContainter}>
-      <ProfileHeader userId={userId} />
+      {/* {loadingUserDetails && <ProfileHeaderLoader />} */}
+      {errorUserDetails && <AlertMessage>{errorUserDetails}</AlertMessage>}
+      {userDetails && (
+        <ProfileHeader
+          userId={userId}
+          profileImage={userDetails.profileImage}
+          name={userDetails.name}
+          username={userDetails.username}
+          bio={userDetails.bio}
+          website={userDetails.website}
+          interests={userDetails.interests}
+          followingCount={userDetails.following.length}
+          followersCount={userDetails.followers.length}
+          isFollowing={userDetails.followers.includes(authUserId)}
+        />
+      )}
+
       <CollectionGrid userId={userId} />
     </View>
   );
