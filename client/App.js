4f6ff7ce1d1447ce198ba90a72d458c584f5bc27
Taliah Wharton <taliahwh23@yahoo.com>
2022-07-12 16:34:22 -0400

    frontend expo push notifications

--- client/App.js
+++ client/App.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider, useSelector, useDispatch } from 'react-redux';
 import { MenuProvider } from 'react-native-popup-menu';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './src/store';
 import { LogBox } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+// Helper Functions
+import getNotificationsPermission from './src/utils/getNotificationsPermission';
 
 // Components
 import Loader from './src/components/Loader';
@@ -13,7 +17,51 @@ import Loader from './src/components/Loader';
 import { navigationRef } from './src/navigation/RootNavigation';
 import MainNavigator from './src/navigation/MainNavigator';
 import AuthNavigator from './src/navigation/AuthNavigator';
-import { getUserDetails } from './src/actions/userActions';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: true,
+  }),
+});
+
+const App = () => {
+  const { authToken } = useSelector((state) => state.userSignIn);
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    LogBox.ignoreLogs([
+      `ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from 'deprecated-react-native-prop-types'.`,
+    ]);
+
+    getNotificationsPermission();
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener(() => {});
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return (
+    <NavigationContainer ref={navigationRef}>
+      {authToken === null ? <AuthNavigator /> : <MainNavigator />}
+    </NavigationContainer>
+  );
+};
 
 const AppWrapper = () => {
   return (
@@ -24,23 +72,6 @@ const AppWrapper = () => {
         </MenuProvider>
       </PersistGate>
     </Provider>
-  );
-};
-
-const App = () => {
-  const dispatch = useDispatch();
-
-  const { authToken } = useSelector((state) => state.userSignIn);
-  useEffect(() => {
-    LogBox.ignoreLogs([
-      `ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from 'deprecated-react-native-prop-types'.`,
-    ]);
-  }, []);
-
-  return (
-    <NavigationContainer ref={navigationRef}>
-      {authToken === null ? <AuthNavigator /> : <MainNavigator />}
-    </NavigationContainer>
   );
 };
 
