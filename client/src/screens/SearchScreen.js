b8ee03fb7d3fd4a585937b572e7b8fdbd47453b8
Taliah Wharton <taliahwh23@yahoo.com>
2022-07-22 16:37:51 -0400

    testing

--- client/src/screens/SearchScreen.js
+++ client/src/screens/SearchScreen.js
@@ -6,9 +6,11 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  Image,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as Notifications from 'expo-notifications';
+import { launchImageLibrary } from 'react-native-image-picker';
 
 const SearchScreen = () => {
   const handlePushNotification = async () => {
@@ -20,6 +22,23 @@ const SearchScreen = () => {
       },
       trigger: { seconds: 2 },
     });
+  };
+
+  const handleUpload = async () => {
+    const options = {
+      title: 'Select Image',
+      type: 'library',
+      options: {
+        maxHeight: 200,
+        maxWidth: 200,
+        selectionLimit: 1,
+        mediaType: 'photo',
+        includeBase64: false,
+      },
+    };
+
+    const image = await launchImageLibrary(options);
+    console.log(image);
   };
 
   return (
@@ -45,6 +64,13 @@ const SearchScreen = () => {
       <TouchableOpacity onPress={handlePushNotification}>
         <Text style={styles.notificationBtn}>Push Notification</Text>
       </TouchableOpacity>
+      <TouchableOpacity onPress={handleUpload}>
+        <Text style={styles.uploadBtn}>Upload</Text>
+      </TouchableOpacity>
+      {/* <Image
+        // source={require('../../../uploads/image-1658343811184.jpg')}
+        style={{ width: 50, height: 50 }}
+      /> */}
     </SafeAreaView>
   );
 };
@@ -86,6 +112,19 @@ const styles = StyleSheet.create({
     width: 150,
     textAlign: 'center',
     backgroundColor: 'gray',
+    padding: 10,
+    fontWeight: '500',
+    color: 'white',
+    fontSize: 15,
+  },
+  uploadBtn: {
+    marginTop: 20,
+    marginLeft: 125,
+    display: 'flex',
+    justifyContent: 'center',
+    width: 150,
+    textAlign: 'center',
+    backgroundColor: 'black',
     padding: 10,
     fontWeight: '500',
     color: 'white',
