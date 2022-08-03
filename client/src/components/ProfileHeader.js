b9ff32d160b9ebf44c14070cbee0a533490c5a69
Taliah Wharton <taliahwh23@yahoo.com>
2022-08-03 11:07:06 -0400

    global styling variables implemented

--- client/src/components/ProfileHeader.js
+++ client/src/components/ProfileHeader.js
@@ -19,6 +19,10 @@ import {
   MenuTrigger,
 } from 'react-native-popup-menu';
 
+// Styles
+import styles from '../styles/ProfileHeaderStyles';
+import theme from '../styles/styles.theme';
+
 // Actions
 import { logout, followUser } from '../actions/userActions';
 
@@ -74,11 +78,11 @@ const ProfileHeader = ({
           <Text style={styles.userDisplayName}>{name}</Text>
           <Text style={styles.username}>{`@${username}`}</Text>
           <View style={styles.ratingsContainer}>
-            <Ionicons name="star" size={15} color="#3E5E7E" />
-            <Ionicons name="star" size={15} color="#3E5E7E" />
-            <Ionicons name="star" size={15} color="#3E5E7E" />
-            <Ionicons name="star" size={15} color="#3E5E7E" />
-            <Ionicons name="star" size={15} color="#3E5E7E" />
+            <Ionicons name="star" size={15} color={theme.PRIMARY_COLOR} />
+            <Ionicons name="star" size={15} color={theme.PRIMARY_COLOR} />
+            <Ionicons name="star" size={15} color={theme.PRIMARY_COLOR} />
+            <Ionicons name="star" size={15} color={theme.PRIMARY_COLOR} />
+            <Ionicons name="star" size={15} color={theme.PRIMARY_COLOR} />
             <Text>(13)</Text>
           </View>
         </View>
@@ -173,126 +177,5 @@ const ProfileHeader = ({
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-  },
-  userInfoContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  userImage: {
-    flex: 2,
-    // backgroundColor: 'pink',
-    height: 68,
-    borderWidth: 1,
-    borderColor: '#d4d4d4',
-    borderRadius: 1000,
-    // marginBottom: 5,
-  },
-
-  userNameContainer: {
-    flex: 8,
-    // backgroundColor: 'orange',
-    display: 'flex',
-    marginLeft: 15,
-    justifyContent: 'space-between',
-  },
-  userDisplayName: {
-    fontSize: 20,
-    fontWeight: '700',
-  },
-  username: {
-    fontSize: 15,
-    color: '#737373',
-  },
-  ratingsContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    // backgroundColor: 'pink',
-    width: '40%',
-    justifyContent: 'space-between',
-  },
-  tagsContainer: {
-    // backgroundColor: 'pink',
-    marginTop: 10,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  lookingFor: { marginRight: 7 },
-  tags: {
-    marginRight: 10,
-    fontSize: 13,
-    fontWeight: '500',
-    color: '#a3a3a3',
-    backgroundColor: '#F1F1F1',
-    paddingHorizontal: 7,
-    paddingVertical: 3,
-    borderRadius: 10,
-    overflow: 'hidden',
-  },
-  description: {
-    marginTop: 10,
-    // fontSize: 15,
-  },
-  followersAndShareContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    paddingTop: 10,
-    paddingBottom: 3,
-  },
-  followersContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    flex: 4,
-    justifyContent: 'space-between',
-  },
-  followBtnContainer: {
-    flex: 6,
-    marginLeft: 25,
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  followBtn: {
-    backgroundColor: '#3E5E7E',
-    width: '100%',
-    textAlign: 'center',
-    paddingVertical: 5,
-    fontWeight: '600',
-    fontSize: 15,
-    borderRadius: 3,
-    overflow: 'hidden',
-    color: '#fff',
-  },
-  followingBtn: {
-    backgroundColor: '#fff',
-    borderWidth: 1,
-    borderColor: '#3E5E7E',
-    width: '100%',
-    textAlign: 'center',
-    paddingVertical: 5,
-    fontWeight: '600',
-    fontSize: 15,
-    borderRadius: 3,
-    overflow: 'hidden',
-    color: '#3E5E7E',
-  },
-  count: {
-    fontWeight: '600',
-  },
-  menu: {
-    borderRadius: 10,
-    padding: 7,
-  },
-  website: {
-    color: '#3E5E7E',
-    fontWeight: '500',
-    marginTop: 3,
-  },
-});
 
 export default ProfileHeader;
